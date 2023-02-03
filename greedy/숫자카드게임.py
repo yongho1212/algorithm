8ece2ee63fdddde1abe9cd304eed875fc572e100
yongho <yongho6800@gmail.com>
2023-02-04 02:42:49 +0900

    그리디 알고리즘 3ㅁㄴ

--- greedy/숫자카드게임.py
+++ greedy/숫자카드게임.py
@@ -1,2 +1,12 @@
 n, m = map(int, input().split())
 print(n, m)
+
+result = 0
+
+for i in range(n):
+    data = list(map(int, input().split()))
+    min_value = min(data)
+
+    result = max(result, min_value)
+
+print(result)
---
+++ programmers/lv.0/점의위치구하기.py
@@ -0,0 +1,14 @@
+def solution(dot):
+    if dot[0] < 0 and dot[1] < 0:
+        return 3
+    elif dot[0] < 0 and dot[1] > 0:
+        return 2
+    elif dot[0] > 0 and dot[1] > 0:
+        return 1
+    elif dot[0] > 0 and dot[1] < 0:
+        return 4
+
+
+
+def solution(dot):
+    return [[1, 4], [2, 3]][dot[0] < 0][dot[1] < 0]
