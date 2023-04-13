38174cd39f047d1c6c99bb15dcca6283d1d8ff23
최용호 <61383329+yongho1212@users.noreply.github.com>
2023-04-13 09:25:30 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,8 +1,13 @@
-import math
+
 def solution(brown, yellow):
+    # 총 카펫의 크기
     ssum = brown + yellow
+    # 카펫의 크기가 될 수 있는 경우 탐색 
+    # 노란색이 최소 1이므로 3부터 ~
     for i in range(3, ssum//2 + 1):
+        # 카펫의 크기가 i로 나눠떨어지고 
         if ssum % i == 0 and i >= 2:
+            # 세로 -2 * 가르 -2 가 노란색의 크기와 같은 경우
             if (i - 2) * (ssum//i - 2) == yellow:
                 
                 return [ssum//i, i]
