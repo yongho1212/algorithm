b09ac0fd259fd340ae0d27f0d0ff41ed5b8e0a2e
최용호 <yongho6800@gmail.com>
2023-07-05 09:04:39 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -4,12 +4,12 @@ def solution(k, dungeons):
     ans = 0
 
     # 모든 던전 순열에 대해 탐색
-    for dungeon_perm in permutations(dungeons, len(dungeons)):
+    for i in permutations(dungeons, len(dungeons)):
         hp = k
         count = 0
 
         # 순열의 각 던전을 방문하며 체력을 소모함
-        for dungeon in dungeon_perm:
+        for dungeon in i:
             if hp >= dungeon[0]:
                 hp -= dungeon[1]
                 count += 1
