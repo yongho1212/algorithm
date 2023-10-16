208e5b63a4dd2b1f03926f0fec1940715b6719d2
최용호 <yongho6800@gmail.com>
2023-10-16 16:15:25 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.py
@@ -0,0 +1,14 @@
+def solution(n):
+    result = []
+    while n:
+        t = n % 3
+        if not t:
+            t = 4
+            n -= 1
+        result.append(str(t))
+        n //= 3
+        print(n)
+    return ''.join(result[::-1])
+        
+    
+    
